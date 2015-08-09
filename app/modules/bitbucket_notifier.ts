4c3c036b31075db96f9430c59bf49db56eaa13d9
Jakub Turek <ja@kubaturek.pl>
2015-08-09 21:24:12 +0200

    Init PullRequestComponent

--- app/modules/bitbucket_notifier.ts
+++ app/modules/bitbucket_notifier.ts
@@ -4,4 +4,5 @@ module BitbucketNotifier {
     'use strict';
 
     var application = angular.module('bitbucketNotifier', []);
+    application.directive('pullRequest', PullRequestComponent.factory());
 }
