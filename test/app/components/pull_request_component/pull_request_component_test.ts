958636d7a97a2b6f0f940e178d35d7d8b78ccb4f
Jakub Turek <jakub.turek@edpauto.com>
2015-08-10 19:42:39 +0200

    Introduce 'mode' attribute to PullRequestComponent

--- test/app/components/pull_request_component/pull_request_component_test.ts
+++ test/app/components/pull_request_component/pull_request_component_test.ts
@@ -4,6 +4,7 @@ describe('PullRequestComponent', () => {
     var element,
         $compile: ng.ICompileService,
         $scope: ng.IRootScopeService,
+        pullRequest: BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
 
     beforeEach(module('bitbucketNotifier'));
     beforeEach(module('bitbucketNotifier.templates'));
@@ -26,31 +27,44 @@ describe('PullRequestComponent', () => {
         var userAsReviewer:BitbucketNotifier.User = new BitbucketNotifier.User();
         userAsReviewer.displayName = 'Anna Kowalsky';
 
+        var secondUserAsReviewer:BitbucketNotifier.User = new BitbucketNotifier.User();
+        secondUserAsReviewer.displayName = 'Jack Sparrow';
+
         var reviewer:BitbucketNotifier.Reviewer = new BitbucketNotifier.Reviewer();
         reviewer.user = userAsReviewer;
         reviewer.approved = true;
+
+        var secondReviewer:BitbucketNotifier.Reviewer = new BitbucketNotifier.Reviewer();
+        secondReviewer.user = secondUserAsReviewer;
+        secondReviewer.approved = false;
 
         var project:BitbucketNotifier.Project = new BitbucketNotifier.Project();
         project.name = 'CRM';
         project.fullName = 'dacsoftware/crm';
 
-        var pullRequest:BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
         pullRequest.id = 1;
         pullRequest.title = 'This is a pull request';
         pullRequest.author = author;
         pullRequest.reviewers.push(reviewer);
         pullRequest.targetRepository = project;
         pullRequest.targetBranch = 'master';
-
-        $scope['pullRequest'] = pullRequest;
-
-        element = $compile('<pull-request pr="pullRequest"></pull-request>')($scope);
-        $scope.$digest();
     });
 
-    it("should render basic pull request information", () => {
-        expect(element.html()).toContain('John Smith');
-        expect(element.html()).toContain('CRM');
-        expect(element.html()).toContain('1');
+    describe('Authored mode', () => {
+        beforeEach(() => {
+            $scope['pullRequest'] = pullRequest;
+            $scope['displayMode'] = 'AUTHORED';
+
+            element = $compile('<pull-request pr="pullRequest" mode="displayMode"></pull-request>')($scope);
+            $scope.$digest();
+        });
+
+        it("should render basic pull request information", () => {
+            expect(element.html()).toContain('1');
+            expect(element.html()).toContain('John Smith');
+            expect(element.html()).toContain('This is a pull request');
+            expect(element.html()).toContain('CRM');
+            //expect(element.html()).toContain('1/2');
+        });
     });
 });
