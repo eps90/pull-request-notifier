2cb88f9ecdef0ea13e033cd17e208122f8c7c836
Jakub Turek <ja@kubaturek.pl>
2015-08-10 11:48:42 +0200

    Create simple tests for PullRequesteComponent

---
+++ test/app/components/pull_request_component/pull_request_component_test.ts
@@ -0,0 +1,30 @@
+///<reference path="../../../../app/_typings.ts"/>
+
+describe('PullRequestComponent', () => {
+    beforeEach(module('bitbucketNotifier'));
+    beforeEach(module('bitbucketNotifier.templates'));
+
+    var element,
+        $compile: ng.ICompileService,
+        $rootScope: ng.IRootScopeService,
+        $templateCache: ng.ITemplateCacheService;
+
+    beforeEach(
+        inject([
+            '$compile',
+            '$rootScope',
+            '$templateCache',
+            ($c, $s, $t) => {
+                $compile = $c;
+                $rootScope = $s;
+                $templateCache = $t;
+            }
+        ])
+    );
+
+    it("should render 'Hello world'", () => {
+        element = $compile('<pull-request></pull-request>')($rootScope);
+        $rootScope.$digest();
+        expect(element.html()).toContain('Hello world');
+    });
+});
