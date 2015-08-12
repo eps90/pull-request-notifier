e04c0584215af8dd745969fba0db1c7a0e48f2ef
Jakub Turek <jakub.turek@edpauto.com>
2015-08-12 09:24:09 +0200

    Create simple PullRequestsListComponent

---
+++ test/app/components/pull_requests_list_component/pull_requests_list_component_test.ts
@@ -0,0 +1,69 @@
+///<reference path="../../../../app/_typings.ts"/>
+
+describe('PullRequestsListComponent', () => {
+    beforeEach(module('bitbucketNotifier'));
+    var element,
+        $compile: ng.ICompileService,
+        $scope: ng.IRootScopeService,
+        pullRequests: Array<BitbucketNotifier.PullRequest> = [];
+
+    beforeEach(module('bitbucketNotifier'));
+    beforeEach(module('bitbucketNotifier.templates'));
+
+    beforeEach(
+        inject([
+            '$compile',
+            '$rootScope',
+            ($c, $s) => {
+                $compile = $c;
+                $scope = $s;
+            }
+        ])
+    );
+
+    beforeEach(() => {
+        var author:BitbucketNotifier.User = new BitbucketNotifier.User();
+        author.displayName = 'John Smith';
+
+        var userAsReviewer:BitbucketNotifier.User = new BitbucketNotifier.User();
+        userAsReviewer.displayName = 'Anna Kowalsky';
+
+        var secondUserAsReviewer:BitbucketNotifier.User = new BitbucketNotifier.User();
+        secondUserAsReviewer.displayName = 'Jack Sparrow';
+
+        var reviewer:BitbucketNotifier.Reviewer = new BitbucketNotifier.Reviewer();
+        reviewer.user = userAsReviewer;
+        reviewer.approved = true;
+
+        var secondReviewer:BitbucketNotifier.Reviewer = new BitbucketNotifier.Reviewer();
+        secondReviewer.user = secondUserAsReviewer;
+        secondReviewer.approved = false;
+
+        var project:BitbucketNotifier.Project = new BitbucketNotifier.Project();
+        project.name = 'CRM';
+        project.fullName = 'dacsoftware/crm';
+
+        var pullRequest: BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
+
+        pullRequest.id = 1;
+        pullRequest.title = 'This is a pull request';
+        pullRequest.author = author;
+        pullRequest.reviewers.push(reviewer, secondReviewer);
+        pullRequest.targetRepository = project;
+        pullRequest.targetBranch = 'master';
+
+        pullRequests.push(pullRequest);
+
+        $scope['pullRequests'] = pullRequests;
+        element = $compile('<pull-requests-list pull-requests="pullRequests"></pull-requests-list>')($scope);
+        $scope.$digest();
+    });
+
+    it('should render list of pull requests', () => {
+        expect(element.html()).toContain('1');
+        expect(element.html()).toContain('John Smith');
+        expect(element.html()).toContain('This is a pull request');
+        expect(element.html()).toContain('CRM');
+        expect(element.html()).toContain('1/2');
+    });
+});
