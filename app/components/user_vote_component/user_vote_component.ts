a9d55b8919320fc5757285ccaf8d132f1604a6dc
Jakub Turek <ja@kubaturek.pl>
2015-08-13 22:42:50 +0200

    Create UserVoteComponent

---
+++ app/components/user_vote_component/user_vote_component.ts
@@ -0,0 +1,37 @@
+///<reference path="../../_typings.ts"/>
+
+module BitbucketNotifier {
+    export class UserVoteComponent implements ng.IDirective {
+        constructor(private localStorageService: angular.local.storage.ILocalStorageService) {}
+
+        restrict: string = 'E';
+        scope = {
+            reviewers: '='
+        };
+        templateUrl = '../components/user_vote_component/user_vote_component.html';
+
+        link = (scope: any) => {
+            var classes = ['pr-icon'];
+            for (var reviewerIdx = 0, reviewerLength = scope.reviewers.length; reviewerIdx < reviewerLength; reviewerIdx++) {
+                var reviewer: Reviewer = scope.reviewers[reviewerIdx];
+                if (reviewer.user.username === this.localStorageService.get('app:user')) {
+                    if (reviewer.approved) {
+                        classes.push('fa-check-circle', 'icon-approved');
+                    } else {
+                        classes.push('fa-clock-o', 'icon-waiting');
+                    }
+                    break;
+                }
+            }
+
+            scope.icon = classes.join(' ');
+        };
+
+        static factory() {
+            var directive = (localStorageService) => new UserVoteComponent(localStorageService);
+            directive.$inject = ['localStorageService'];
+
+            return directive;
+        }
+    }
+}
--- app/modules/bitbucket_notifier.ts
+++ app/modules/bitbucket_notifier.ts
@@ -7,6 +7,7 @@ module BitbucketNotifier {
     application.directive('pullRequest', PullRequestComponent.factory());
     application.directive('pullRequestsList', PullRequestsListComponent.factory());
     application.directive('approvalProgress', ApprovalProgressComponent.factory());
+    application.directive('userVote', UserVoteComponent.factory());
 
     application.filter('authored' , AuthoredFilter);
     application.filter('assigned', AssignedFilter);
