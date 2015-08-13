1165971920a22c153cf5a597883c4bb291addd60
Jakub Turek <jakub.turek@edpauto.com>
2015-08-13 21:32:56 +0200

    Create AuthoredFilter filter

--- app/_typings.ts
+++ app/_typings.ts
@@ -3,3 +3,5 @@
 ///<reference path="components/pull_request_component/pull_request_component.ts" />
 ///<reference path="components/pull_requests_list_component/pull_requests_list_component.ts"/>
 ///<reference path="components/approval_progress_component/approval_progress_component.ts"/>
+///<reference path="filters/authored_filter.ts"/>
+
---
+++ test/app/filters/authored_filter_test.ts
@@ -0,0 +1,55 @@
+///<reference path="../../../app/_typings.ts"/>
+
+describe('AuthoredFilter', () => {
+    var $filter,
+        localStorageService: angular.local.storage.ILocalStorageService,
+        pullRequests: Array<BitbucketNotifier.PullRequest>,
+        authoredFilter;
+
+    beforeEach(module('bitbucketNotifier'));
+    beforeEach(inject([
+        '$filter',
+        'localStorageService',
+        ($f, $l) => {
+            $filter = $f;
+            localStorageService = $l;
+        }
+    ]));
+
+    beforeEach(() => {
+        var loggedInUser: BitbucketNotifier.User = new BitbucketNotifier.User();
+        loggedInUser.username = 'john.smith';
+
+        var anotherUser: BitbucketNotifier.User = new BitbucketNotifier.User();
+        anotherUser.username = 'anna.kowalsky';
+
+        var authoredPullRequest: BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
+        authoredPullRequest.id = 101;
+        authoredPullRequest.author = loggedInUser;
+
+        var autherdPullRequestTwo: BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
+        autherdPullRequestTwo.id = 202;
+        autherdPullRequestTwo.author = loggedInUser;
+
+        var anotherPullRequest: BitbucketNotifier.PullRequest = new BitbucketNotifier.PullRequest();
+        anotherPullRequest.id = 303;
+        anotherPullRequest.author = anotherUser;
+
+        pullRequests = [authoredPullRequest, autherdPullRequestTwo, anotherPullRequest];
+        authoredFilter = $filter('authored');
+    });
+
+    it('should include only pull requests authored by logged in user', () => {
+        localStorageService.set('app:user', 'john.smith');
+
+        var result: Array<BitbucketNotifier.PullRequest> = authoredFilter(pullRequests);
+        expect(result.length).toEqual(2);
+        expect(result[0].id).toEqual(101);
+        expect(result[1].id).toEqual(202);
+    });
+
+    it('should return empty set if there are no pull requests authored by a user', () => {
+        localStorageService.set('app:user', 'jon.snow');
+        expect(authoredFilter(pullRequests).length).toEqual(0);
+    });
+});
