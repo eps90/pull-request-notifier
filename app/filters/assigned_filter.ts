9d613caa47fae24fd33d5e0ac0c56c25dfb4f6be
Jakub Turek <jakub.turek@edpauto.com>
2015-08-13 21:49:18 +0200

    Create AssignedFilter filte

---
+++ app/filters/assigned_filter.ts
@@ -0,0 +1,25 @@
+///<reference path="../_typings.ts"/>
+
+module BitbucketNotifier {
+    export function AssignedFilter(localStorageService: angular.local.storage.ILocalStorageService) {
+        this.$inject = ['localStorageService'];
+
+        return (pullRequests: Array<PullRequest>) => {
+            var loggedInUser = localStorageService.get('app:user');
+            var result: Array<PullRequest> = [];
+
+            for (var prIndex = 0, prsLength = pullRequests.length; prIndex < prsLength; prIndex++) {
+                var pullRequest = pullRequests[prIndex];
+                var reviewers = pullRequest.reviewers;
+                for (var reviewerIdx = 0, reviewersLength = reviewers.length; reviewerIdx < reviewersLength; reviewerIdx++) {
+                    var reviewer = reviewers[reviewerIdx];
+                    if (reviewer.user.username === loggedInUser) {
+                        result.push(pullRequest);
+                    }
+                }
+            }
+
+            return result;
+        };
+    }
+}
