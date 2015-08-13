6a7c342a012377f37e3c24913fb5afa6cd816a41
Jakub Turek <ja@kubaturek.pl>
2015-08-13 21:31:47 +0200

    Inject LocalStorageModule into bitbucketNotifier

--- app/modules/bitbucket_notifier.ts
+++ app/modules/bitbucket_notifier.ts
@@ -3,7 +3,7 @@
 module BitbucketNotifier {
     'use strict';
 
-    var application = angular.module('bitbucketNotifier', []);
+    var application = angular.module('bitbucketNotifier', ['LocalStorageModule']);
     application.directive('pullRequest', PullRequestComponent.factory());
     application.directive('pullRequestsList', PullRequestsListComponent.factory());
     application.directive('approvalProgress', ApprovalProgressComponent.factory());
