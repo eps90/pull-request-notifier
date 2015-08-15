3975ded6d632301e7e733981beac08587fb8519f
Jakub Turek <jakub.turek@edpauto.com>
2015-08-15 23:47:45 +0200

    Add additional methods to Notifier service

--- app/services/notifier.ts
+++ app/services/notifier.ts
@@ -10,5 +10,23 @@ module BitbucketNotifier {
         notify(opts) {
             this.chrome.notifications.create(opts);
         }
+
+        notifyNewPullRequestAssigned(pullRequest: PullRequest) {
+            var options = {
+                title: 'New pull request assigned to you!',
+                message: pullRequest.title
+            };
+
+            this.notify(options);
+        }
+
+        notifyPullRequestMerged(pullRequest: PullRequest) {
+            var options = {
+                title: 'Your pull request has been merged',
+                message: pullRequest.title
+            };
+
+            this.notify(options);
+        }
     }
 }
--- test/app/services/notifier_test.ts
+++ test/app/services/notifier_test.ts
@@ -22,4 +22,34 @@ fdescribe('Notifier', () => {
         notifier.notify(notificationOptions);
         expect(window['chrome'].notifications.create).toHaveBeenCalledWith(notificationOptions);
     });
+
+    it('should notify about new pull request', () => {
+        spyOn(window['chrome'].notifications, 'create');
+
+        var pullRequest = new BitbucketNotifier.PullRequest();
+        pullRequest.title = 'This is some title';
+
+        var expectedNotificationOpts = {
+            title: 'New pull request assigned to you!',
+            message: pullRequest.title
+        };
+
+        notifier.notifyNewPullRequestAssigned(pullRequest);
+        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedNotificationOpts);
+    });
+
+    it('should notify about merged pull request', () => {
+        spyOn(window['chrome'].notifications, 'create');
+
+        var pullRequest = new BitbucketNotifier.PullRequest();
+        pullRequest.title = 'This is some title';
+
+        var expectedNotificationOpts = {
+            title: 'Your pull request has been merged',
+            message: pullRequest.title
+        };
+
+        notifier.notifyPullRequestMerged(pullRequest);
+        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedNotificationOpts);
+    });
 });
