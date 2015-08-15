2bb394c9b43714b75eedc2e57e21f1d49724945d
Jakub Turek <jakub.turek@edpauto.com>
2015-08-15 23:59:44 +0200

    Add default options to Notifier

--- test/app/services/notifier_test.ts
+++ test/app/services/notifier_test.ts
@@ -1,7 +1,14 @@
 ///<reference path="../../../app/_typings.ts"/>
 
 fdescribe('Notifier', () => {
-    var notifier: BitbucketNotifier.Notifier;
+    var notifier: BitbucketNotifier.Notifier,
+        expectedOptions = {
+            type: 'basic',
+            iconUrl: '../assets/img/bitbucket_logo_raster.jpg',
+            title: '',
+            message: '',
+            priority: 2
+        };
 
     beforeEach(module('bitbucketNotifier'));
     beforeEach(() => {
@@ -16,11 +23,10 @@ fdescribe('Notifier', () => {
 
     it('should create notificaion via Chrome API', () => {
         spyOn(window['chrome'].notifications, 'create');
-        var notificationOptions = {
-            title: 'Test title'
-        };
-        notifier.notify(notificationOptions);
-        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(notificationOptions);
+
+        expectedOptions.title = 'Test title';
+        notifier.notify(expectedOptions);
+        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedOptions);
     });
 
     it('should notify about new pull request', () => {
@@ -29,13 +35,11 @@ fdescribe('Notifier', () => {
         var pullRequest = new BitbucketNotifier.PullRequest();
         pullRequest.title = 'This is some title';
 
-        var expectedNotificationOpts = {
-            title: 'New pull request assigned to you!',
-            message: pullRequest.title
-        };
+        expectedOptions.title = 'New pull request assigned to you!';
+        expectedOptions.message = pullRequest.title;
 
         notifier.notifyNewPullRequestAssigned(pullRequest);
-        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedNotificationOpts);
+        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedOptions);
     });
 
     it('should notify about merged pull request', () => {
@@ -43,13 +47,10 @@ fdescribe('Notifier', () => {
 
         var pullRequest = new BitbucketNotifier.PullRequest();
         pullRequest.title = 'This is some title';
-
-        var expectedNotificationOpts = {
-            title: 'Your pull request has been merged',
-            message: pullRequest.title
-        };
+        expectedOptions.title = 'Your pull request has been merged';
+        expectedOptions.message = pullRequest.title;
 
         notifier.notifyPullRequestMerged(pullRequest);
-        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedNotificationOpts);
+        expect(window['chrome'].notifications.create).toHaveBeenCalledWith(expectedOptions);
     });
 });
