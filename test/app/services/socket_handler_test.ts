4e98f2d30b686536feb320d0a66270b104328d30
Jakub Turek <jakub.turek@edpauto.com>
2015-08-16 11:33:18 +0200

    Notify about new pull requests assignment

--- test/app/services/socket_handler_test.ts
+++ test/app/services/socket_handler_test.ts
@@ -4,19 +4,27 @@ describe('SocketHandler', () => {
     var socketHandler,
         socket,
         localStorageService: angular.local.storage.ILocalStorageService,
-        pullRequestRepository: BitbucketNotifier.PullRequestRepository;
+        pullRequestRepository: BitbucketNotifier.PullRequestRepository,
+        notifier: BitbucketNotifier.Notifier;
 
     beforeEach(module('bitbucketNotifier'));
+    beforeEach(module(['$provide', ($p: ng.auto.IProvideService) => {
+        $p.value('Notifier', {
+            notifyNewPullRequestAssigned: jasmine.createSpy('notifyNewPullRequestAssigned')
+        });
+    }]));
     beforeEach(inject([
         'SocketHandler',
         'Socket',
         'localStorageService',
         'PullRequestRepository',
-        (sh, s, l, prr) => {
+        'Notifier',
+        (sh, s, l, prr, n) => {
             socketHandler = sh;
             socket = s;
             localStorageService = l;
             pullRequestRepository = prr;
+            notifier = n;
         }
     ]));
     beforeEach(() => {
@@ -40,5 +48,38 @@ describe('SocketHandler', () => {
 
         expect(pullRequestRepository.pullRequests.length).toBe(1);
         expect(pullRequestRepository.pullRequests[0]).toEqual(pullRequest);
+    });
+
+    fdescribe('chrome notifications', () => {
+        var pullRequestEvent: BitbucketNotifier.PullRequestEvent;
+        var pullRequest: BitbucketNotifier.PullRequest;
+        beforeEach(() => {
+            pullRequest = new BitbucketNotifier.PullRequest();
+            pullRequestEvent = new BitbucketNotifier.PullRequestEvent();
+            pullRequestEvent.pullRequests = [pullRequest];
+            pullRequestEvent.context = pullRequest;
+
+            var loggedInUser = new BitbucketNotifier.User();
+            loggedInUser.username = 'john.smith';
+            loggedInUser.displayName = 'John Smith';
+
+            var prAuthor = new BitbucketNotifier.User();
+            prAuthor.displayName = 'Anna Kowalsky';
+            prAuthor.username = 'anna.kowalsky';
+
+            var loggedInReviewer = new BitbucketNotifier.Reviewer();
+            loggedInReviewer.user = loggedInUser;
+            loggedInReviewer.approved = false;
+
+            pullRequest.author = prAuthor;
+            pullRequest.reviewers.push(loggedInReviewer);
+        });
+
+        it('should notify about new merge request assignment', () => {
+            pullRequestEvent.triggeredEvent = 'pullrequest:created';
+
+            socket.receive('server:pullrequests:updated', pullRequestEvent);
+            expect(notifier.notifyNewPullRequestAssigned).toHaveBeenCalledWith(pullRequest)
+        });
     });
 });
