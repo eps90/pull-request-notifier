7493807f2d644d567486ff308ac275fe87234aa6
Jakub Turek <ja@kubaturek.pl>
2015-08-16 22:47:02 +0200

    Create basic Config class

---
+++ app/services/config.ts
@@ -0,0 +1,12 @@
+///<reference path="../_typings.ts"/>
+
+module BitbucketNotifier {
+    export class Config {
+        static $inject = ['localStorageService'];
+
+        constructor(private localStorageService: angular.local.storage.ILocalStorageService) {}
+        getParameter(key: string) {
+            return this.localStorageService.get(key);
+        }
+    }
+}
