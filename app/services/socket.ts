033ca9d9ecd7558620e10204e0d7b32e16eba220
Jakub Turek <ja@kubaturek.pl>
2015-08-18 18:37:59 +0200

    Change Socket service to function

--- app/services/socket.ts
+++ app/services/socket.ts
@@ -1,17 +1,12 @@
 ///<reference path="../_typings.ts"/>
 
 module BitbucketNotifier {
-    export class Socket {
-        constructor(private socketFactory) {}
-
-        static $inject = ['socketFactory'];
-
-        factory() {
-            // @todo Get host and port from config
-            var socket = io.connect('http://localhost:8765');
-            return this.socketFactory({
-                ioSocket: socket
-            });
-        }
+    export function Socket(socketFactory) {
+        this.$inject = ['socketFactory'];
+        // @todo Get host and port from config
+        var socket = io.connect('http://localhost:8765');
+        return socketFactory({
+            ioSocket: socket
+        });
     }
 }
