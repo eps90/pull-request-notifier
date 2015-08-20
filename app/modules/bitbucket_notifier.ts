531b6ca6b6b2f938e5891d98d109e98a776ffa99
Jakub Turek <jakub.turek@edpauto.com>
2015-08-20 20:13:55 +0200

    Add ui.bootrap to bitbucketNotifier module

--- app/modules/bitbucket_notifier.ts
+++ app/modules/bitbucket_notifier.ts
@@ -3,7 +3,7 @@
 module BitbucketNotifier {
     'use strict';
 
-    var application = angular.module('bitbucketNotifier', ['LocalStorageModule', 'btford.socket-io']);
+    var application = angular.module('bitbucketNotifier', ['LocalStorageModule', 'btford.socket-io', 'ui.bootstrap']);
     application.directive('pullRequest', PullRequestComponent.factory());
     application.directive('pullRequestsList', PullRequestsListComponent.factory());
     application.directive('approvalProgress', ApprovalProgressComponent.factory());
