7e93fb8874bbce33aa361c5bf1aa8ad35b74adb6
Jakub Turek <jakub.turek@edpauto.com>
2015-08-21 21:43:28 +0200

    Do not pass MODE to PullRequestsList

--- app/components/pull_requests_list_component/pull_requests_list_component.ts
+++ app/components/pull_requests_list_component/pull_requests_list_component.ts
@@ -3,9 +3,6 @@
 module BitbucketNotifier {
     export class PullRequestsListComponent implements ng.IDirective {
         restrict: string = 'E';
-        scope: any = {
-            mode: '='
-        };
         templateUrl: string = '../components/pull_requests_list_component/pull_requests_list_component.html';
 
         constructor(private pullRequestRepository: PullRequestRepository) {}
