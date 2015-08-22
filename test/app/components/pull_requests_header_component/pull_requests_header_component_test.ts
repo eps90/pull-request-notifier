de5c2f3f9b8d589d7f82a8847c8884bb2bc73955
Jakub Turek <jakub.turek@edpauto.com>
2015-08-22 11:47:00 +0200

    Create PullRequestsHeaderComponent

---
+++ test/app/components/pull_requests_header_component/pull_requests_header_component_test.ts
@@ -0,0 +1,40 @@
+///<reference path="../../../../app/_typings.ts"/>
+
+describe('PullRequestsHeaderComponent', () => {
+    var $scope: ng.IScope,
+        $compile: ng.ICompileService,
+        element;
+
+    beforeEach(module('bitbucketNotifier'));
+    beforeEach(module('bitbucketNotifier.templates'));
+    beforeEach(inject([
+        '$rootScope',
+        '$compile',
+        ($s, $c) => {
+            $scope = $s;
+            $compile = $c;
+        }
+    ]));
+
+    it('should display header for authored mode', () => {
+        element = $compile('<pull-requests-header mode="AUTHORED"></pull-requests-header>')($scope);
+        $scope.$digest();
+
+        expect(element.text()).toContain('Id');
+        expect(element.text()).toContain('Title');
+        expect(element.text()).toContain('Project');
+        expect(element.text()).toContain('Author');
+        expect(element.text()).toContain('Progress');
+    });
+
+    it('should display header for assigned mode', () => {
+        element = $compile('<pull-requests-header mode="ASSIGNED"></pull-requests-header>')($scope);
+        $scope.$digest();
+
+        expect(element.text()).toContain('Id');
+        expect(element.text()).toContain('Title');
+        expect(element.text()).toContain('Project');
+        expect(element.text()).toContain('Author');
+        expect(element.text()).toContain('Voting');
+    });
+});
