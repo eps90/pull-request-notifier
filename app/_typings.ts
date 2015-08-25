1e3e54ad2b75223451595ec96724e52edf27430c
Jakub Turek <ja@kubaturek.pl>
2015-08-25 20:22:57 +0200

    Create simple options component

--- app/_typings.ts
+++ app/_typings.ts
@@ -12,6 +12,7 @@
 ///<reference path="components/pull_requests_header_component/pull_requests_header_component.ts"/>
 ///<reference path="components/approval_progress_component/approval_progress_component.ts"/>
 ///<reference path="components/user_vote_component/user_vote_component.ts"/>
+///<reference path="components/options_component/options_component.ts"/>
 ///<reference path="filters/authored_filter.ts"/>
 ///<reference path="filters/assigned_filter.ts"/>
 
