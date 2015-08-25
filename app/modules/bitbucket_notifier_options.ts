93c1f9ddf2578179ee7534fc5499ee6c01e7ff23
Jakub Turek <ja@kubaturek.pl>
2015-08-25 22:08:22 +0200

    Add missing Config servce in bitbucketNotifier.options

--- app/modules/bitbucket_notifier_options.ts
+++ app/modules/bitbucket_notifier_options.ts
@@ -4,4 +4,5 @@ module BitbucketNotifier {
     var application = angular.module('bitbucketNotifier.options', ['LocalStorageModule']);
 
     application.directive('options', OptionsComponent.factory());
+    application.service('Config', Config);
 }
