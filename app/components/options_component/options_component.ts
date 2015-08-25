7aaf452aa83dea3adcba7a0f58197c9b140d5906
Jakub Turek <ja@kubaturek.pl>
2015-08-25 22:45:15 +0200

    Adapt options component to save data into config

--- app/components/options_component/options_component.ts
+++ app/components/options_component/options_component.ts
@@ -9,6 +9,20 @@ module BitbucketNotifier {
 
         constructor(private config: Config) {}
 
+        link: ng.IDirectiveLinkFn = (scope: ng.IScope) => {
+            scope['options'] = {
+                appUser: this.config.getUsername(),
+                socketServerAddress: this.config.getSocketServerAddress()
+            };
+        };
+
+        controller = ($scope: ng.IScope) => {
+            $scope['saveOptions'] = () => {
+                this.config.setUsername($scope['options'].appUser);
+                this.config.setSocketServerAddress($scope['options'].socketServerAddress);
+            }
+        };
+
         static factory(): ng.IDirectiveFactory {
             var component = (config) => new OptionsComponent(config);
             component.$inject = ['Config'];
