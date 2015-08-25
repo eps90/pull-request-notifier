046a9ca3338cd35fb139e6ddc83e78af4252ee42
Jakub Turek <ja@kubaturek.pl>
2015-08-25 20:23:21 +0200

    Create bitbucketNotifier.options module

---
+++ app/modules/bitbucket_notifier_options.ts
@@ -0,0 +1,7 @@
+///<reference path="../_typings.ts"/>
+
+module BitbucketNotifier {
+    var application = angular.module('bitbucketNotifier.options', ['LocalStorageModule']);
+
+    application.directive('options', OptionsComponent.factory());
+}
