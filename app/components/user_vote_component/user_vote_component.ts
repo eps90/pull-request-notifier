19b788afe2a3a77598780eb35a19a3ecabf8427b
Jakub Turek <ja@kubaturek.pl>
2015-08-26 07:47:25 +0200

    Add tooltip to UserVoteComponent

--- app/components/user_vote_component/user_vote_component.ts
+++ app/components/user_vote_component/user_vote_component.ts
@@ -13,20 +13,24 @@ module BitbucketNotifier {
         templateUrl: string = '../components/user_vote_component/user_vote_component.html';
 
         link: ng.IDirectiveLinkFn = (scope: any) => {
-            var classes = ['pr-icon'];
+            var classes = ['fa'];
+            var label = '';
             for (var reviewerIdx = 0, reviewerLength = scope.reviewers.length; reviewerIdx < reviewerLength; reviewerIdx++) {
                 var reviewer: Reviewer = scope.reviewers[reviewerIdx];
                 if (reviewer.user.username === this.config.getUsername()) {
                     if (reviewer.approved) {
                         classes.push('fa-check-circle', 'icon-approved');
+                        label = 'Approved';
                     } else {
                         classes.push('fa-clock-o', 'icon-waiting');
+                        label = 'Waiting';
                     }
                     break;
                 }
             }
 
             scope.icon = classes.join(' ');
+            scope.label = label;
         };
 
         static factory(): ng.IDirectiveFactory {
