f131488901ee937cf3f278c6925481a6c2c499d3
Jakub Turek <jakub.turek@edpauto.com>
2015-09-02 22:11:36 +0200

    Go to Bitbucket page on click on logo

--- app/components/navigation_bar_component/navigation_bar_component.ts
+++ app/components/navigation_bar_component/navigation_bar_component.ts
@@ -5,12 +5,19 @@ module BitbucketNotifier {
         restrict: string = 'E';
         templateUrl: string = '../components/navigation_bar_component/navigation_bar_component.html';
 
+        constructor(private bitbucketUrl: string) {}
+
         link: ng.IDirectiveLinkFn = (scope: ng.IScope) => {
             scope['appVersion'] = 'v' + window['chrome'].runtime.getManifest().version;
+            scope['goToBitbucket'] = () => {
+                window['chrome'].tabs.create({url: this.bitbucketUrl});
+            }
         };
 
         static factory(): ng.IDirectiveFactory {
-            return () => new NavigationBarComponent();
+            var component = (bitbucketUrl) => new NavigationBarComponent(bitbucketUrl);
+            component.$inject = ['bitbucketUrl'];
+            return component;
         }
     }
 }
