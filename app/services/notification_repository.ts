2c036203e0e8e4f55d97e11007a02e88e4eb23ca
Jakub Turek <ja@kubaturek.pl>
2015-09-02 12:37:54 +0200

    Split repositories into separate files

---
+++ app/services/notification_repository.ts
@@ -0,0 +1,30 @@
+///<reference path="../_typings.ts"/>
+
+module BitbucketNotifier {
+    export class NotificationRepository {
+        private notifications: Array<Notification> = [];
+
+        add(notificationId: string, pullRequestLink: string): void {
+            var prNotification = new PullRequestNotification();
+            prNotification.notificationId = notificationId;
+            prNotification.pullRequestHtmlLink = pullRequestLink;
+
+            this.notifications.push(prNotification);
+        }
+
+        getAll(): Array<Notification> {
+            return this.notifications;
+        }
+
+        find(notificationId: string): Notification {
+            for (var notifIdx = 0, notifLen = this.notifications.length; notifIdx < notifLen; notifIdx++) {
+                var notification = this.notifications[notifIdx];
+                if (notification.notificationId === notificationId) {
+                    return notification;
+                }
+            }
+
+            return new PullRequestNotification();
+        }
+    }
+}
