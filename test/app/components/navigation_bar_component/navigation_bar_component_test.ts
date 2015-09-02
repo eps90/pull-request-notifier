262691db4183a11bd09fb221cbc7ba6d29556392
Jakub Turek <ja@kubaturek.pl>
2015-09-02 22:11:36 +0200

    Go to Bitbucket page on click on logo

--- test/app/components/navigation_bar_component/navigation_bar_component_test.ts
+++ test/app/components/navigation_bar_component/navigation_bar_component_test.ts
@@ -4,7 +4,8 @@ describe("NavigationBarComponent", () => {
     var $scope: ng.IScope,
         $compile: ng.ICompileService,
         element: ng.IAugmentedJQuery,
-        appVersion = '1.0.0';
+        appVersion = '1.0.0',
+        bitbucketUrl = 'http://example.com';
 
     beforeEach(module('bitbucketNotifier'));
     beforeEach(module('bitbucketNotifier.templates'));
@@ -12,9 +13,18 @@ describe("NavigationBarComponent", () => {
         window['chrome'] = {
             runtime: {
                 getManifest: jasmine.createSpy('chrome.runtime.getManifest').and.returnValue({version: appVersion})
+            },
+            tabs: {
+                create: jasmine.createSpy('chrome.tabs.create')
             }
         }
     ]);
+    beforeEach(module([
+        '$provide',
+        ($provide: ng.auto.IProvideService) => {
+            $provide.value('bitbucketUrl', bitbucketUrl);
+        }
+    ]));
     beforeEach(inject([
         '$compile',
         '$rootScope',
@@ -31,5 +41,15 @@ describe("NavigationBarComponent", () => {
         var versionField = element.find('.app-version');
         var expectedAppVersion = 'v' + appVersion;
         expect(versionField.text()).toEqual(expectedAppVersion);
+    });
+
+    it('should have link following to Bitbucket page', () => {
+        element = $compile('<navigation-bar></navigation-bar>')($scope);
+        $scope.$digest();
+
+        var linkElement = element.find('a.bitbucket-logo');
+        linkElement.triggerHandler('click');
+
+        expect(window['chrome'].tabs.create).toHaveBeenCalledWith({url: bitbucketUrl});
     });
 });
