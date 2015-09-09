4f9be29d25a7d6862e9c8cfa57355ad044321b3e
Jakub Turek <jakub.turek@edpauto.com>
2015-09-09 20:11:36 +0200

    Create PullRequestFactory

--- app/services/factories.ts
+++ app/services/factories.ts
@@ -28,4 +28,76 @@ module BitbucketNotifier {
             return project;
         }
     }
+
+    export class PullRequestFactory {
+        static create(rawObject: any): PullRequest {
+            var pullRequest = new PullRequest();
+            if (rawObject.hasOwnProperty('id')) {
+                pullRequest.id = rawObject.id;
+            }
+
+            if (rawObject.hasOwnProperty('title')) {
+                pullRequest.title = rawObject.title;
+            }
+
+            if (rawObject.hasOwnProperty('description')) {
+                pullRequest.description = rawObject.description;
+            }
+
+            if (rawObject.hasOwnProperty('targetBranch')) {
+                pullRequest.targetBranch = rawObject.targetBranch;
+            }
+
+            if (rawObject.hasOwnProperty('author')) {
+                pullRequest.author = UserFactory.create(rawObject.author);
+            }
+
+            if (rawObject.hasOwnProperty('targetRepository')) {
+                pullRequest.targetRepository = ProjectFactory.create(rawObject.targetRepository);
+            }
+
+            if (rawObject.hasOwnProperty('reviewers') && Array.isArray(rawObject.reviewers)) {
+                pullRequest.reviewers = [];
+                for (var ridx = 0, rlen = rawObject.reviewers.length; ridx < rlen; ridx++) {
+                    var targetReviewer = new Reviewer();
+                    var currentReviewer = rawObject.reviewers[ridx];
+                    if (currentReviewer.hasOwnProperty('approved')) {
+                        targetReviewer.approved = currentReviewer.approved;
+                    }
+
+                    if (currentReviewer.hasOwnProperty('user')) {
+                        targetReviewer.user = UserFactory.create(currentReviewer.user);
+                    }
+
+                    pullRequest.reviewers.push(targetReviewer);
+                }
+            }
+
+            if (rawObject.hasOwnProperty('links')) {
+                if (rawObject.links.hasOwnProperty('self')) {
+                    pullRequest.links.self = rawObject.links.self;
+                }
+
+                if (rawObject.links.hasOwnProperty('html')) {
+                    pullRequest.links.html = rawObject.links.html;
+                }
+            }
+
+            if (rawObject.hasOwnProperty('state')) {
+                switch (rawObject.state) {
+                    case 0:
+                        pullRequest.state = PullRequestState.Open;
+                        break;
+                    case 1:
+                        pullRequest.state = PullRequestState.Merged;
+                        break;
+                    case 2:
+                        pullRequest.state = PullRequestState.Declined;
+                        break;
+                }
+            }
+
+            return pullRequest;
+        }
+    }
 }
