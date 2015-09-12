b6a558c4f3c02cdf3f481b6b97758781f4561b82
Jakub Turek <ja@kubaturek.pl>
2015-09-12 15:35:30 +0200

    Show approval progress depending on configuration

--- test/app/components/approval_progress_component/approval_progress_component_test.ts
+++ test/app/components/approval_progress_component/approval_progress_component_test.ts
@@ -3,16 +3,32 @@
 describe('ApprovalProgressComponent', () => {
     var $scope: ng.IScope,
         $compile: ng.ICompileService,
-        element;
+        config: BitbucketNotifier.Config,
+        element,
+        prProgress = null;
 
     beforeEach(module('bitbucketNotifier'));
     beforeEach(module('bitbucketNotifier.templates'));
+    beforeEach(module([
+        '$provide',
+        ($provide: ng.auto.IProvideService) => {
+            prProgress = BitbucketNotifier.PullRequestProgress.PROPORTIONS;
+
+            $provide.value('Config', {
+                getPullRequestProgress: jasmine.createSpy('Config.getPullRequestProgress').and.callFake(() => {
+                    return prProgress;
+                })
+            });
+        }
+    ]));
     beforeEach(inject([
         '$compile',
         '$rootScope',
-        ($c, $r) => {
+        'Config',
+        ($c, $r, c) => {
             $compile = $c;
             $scope = $r;
+            config = c;
         }
     ]));
 
@@ -38,5 +54,50 @@ describe('ApprovalProgressComponent', () => {
         $scope.$digest();
 
         expect(element.find('i.icon-no-assigned').length).toBe(1);
+    });
+
+    describe('Appearance variants', () => {
+        beforeEach(() => {
+            var approvedReviewer = new BitbucketNotifier.Reviewer();
+            approvedReviewer.approved = true;
+            var unapprovedReviewer = new BitbucketNotifier.Reviewer();
+            unapprovedReviewer.approved = false;
+            var anotherUnapprovedReviewer = new BitbucketNotifier.Reviewer();
+            anotherUnapprovedReviewer.approved = false;
+
+            $scope['reviewers'] = [approvedReviewer, unapprovedReviewer, anotherUnapprovedReviewer];
+        });
+
+        it('should set up currently set pull request progress option', () => {
+            element = $compile('<approval-progress reviewers="reviewers"></approval-progress>')($scope);
+            $scope.$digest();
+
+            expect(element.isolateScope()['pullRequestProgress']).toEqual(prProgress);
+        });
+
+        it("should display proportions value if 'proportions' is set as progress option", () => {
+            prProgress = BitbucketNotifier.PullRequestProgress.PROPORTIONS;
+            element = $compile('<approval-progress reviewers="reviewers"></approval-progress>')($scope);
+            $scope.$digest();
+
+            expect(element.text()).toContain('1/3');
+        });
+
+        it("should display percentage value if 'percentage' is set as progress option", () => {
+            prProgress = BitbucketNotifier.PullRequestProgress.PERCENT;
+            element = $compile('<approval-progress reviewers="reviewers"></approval-progress>')($scope);
+            $scope.$digest();
+
+            expect(element.text()).toContain('33%');
+        });
+
+        it("should display progress bar of 'progress_bar' is set as progress option", () => {
+            prProgress = BitbucketNotifier.PullRequestProgress.PROGRESS_BAR;
+            element = $compile('<approval-progress reviewers="reviewers"></approval-progress>')($scope);
+            $scope.$digest();
+
+            expect(element.find('.progress').length).toEqual(1);
+            expect(element.find('.progress-bar').css('width')).toEqual('33%');
+        });
     });
 });
