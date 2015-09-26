571b0484a14123007c01eae840b8ac692f94c644
Jakub Turek <jakub.turek@edpauto.com>
2015-09-26 11:58:26 +0200

    Create basic SoundManager

---
+++ app/services/sound_manager.ts
@@ -0,0 +1,37 @@
+///<reference path="../_typings.ts"/>
+
+module BitbucketNotifier {
+    'use strict';
+
+    export class SoundManager {
+
+        static $inject: string[] = ['Config'];
+
+        constructor(private config: Config) {
+            createjs.Sound.registerSound(config.getNewPullRequestSound(), Sound.NEW_PULLREQUEST);
+            createjs.Sound.registerSound(config.getApprovedPullRequestSound(), Sound.APPROVED_PULLREQUEST);
+            createjs.Sound.registerSound(config.getMergedPullRequestSound(), Sound.MERGED_PULLREQUEST);
+            createjs.Sound.registerSound(config.getReminderSound(), Sound.REMINDER);
+        }
+
+        play(soundId: string): void {
+            createjs.Sound.play(soundId);
+        }
+
+        playNewPullRequestSound(): void {
+            createjs.Sound.play(Sound.NEW_PULLREQUEST);
+        }
+
+        playApprovedPullRequestSound(): void {
+            createjs.Sound.play(Sound.APPROVED_PULLREQUEST);
+        }
+
+        playMergedPullRequestSound(): void {
+            createjs.Sound.play(Sound.MERGED_PULLREQUEST);
+        }
+
+        playReminderSound(): void {
+            createjs.Sound.play(Sound.REMINDER);
+        }
+    }
+}
