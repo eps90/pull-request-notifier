7e63e492f540cffb978a0de5efe1a8bb24adccba
Jakub Turek <ja@kubaturek.pl>
2015-11-21 18:28:26 +0100

    Create PullRequestPreview component

---
+++ app/components/pull_request_preview_component/pull_request_preview_component.ts
@@ -0,0 +1,17 @@
+///<reference path="../../_typings.ts"/>
+
+module BitbucketNotifier {
+    'use strict';
+
+    export class PullRequestPreviewComponent implements ng.IDirective {
+        restrict: string = 'E';
+        templateUrl: string = '../components/pull_request_preview_component/pull_request_preview_component.html';
+        scope: any = {
+            pr: '='
+        };
+
+        static factory(): ng.IDirectiveFactory {
+            return () => new PullRequestPreviewComponent();
+        }
+    }
+}
