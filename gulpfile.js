070194045924cbd9c75fe2878c65f72675041706
Jakub Turek <ja@kubaturek.pl>
2016-01-30 11:01:54 +0100

    Copy all necessary files into build directory during dist task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var del = require('del');
 var typescript = require('gulp-typescript');
 var karmaServer = require('karma').Server;
+var merge = require('merge-stream');
+var flatten = require('gulp-flatten');
 
 gulp.task('clean:test', function () {
     return del(['build/*', 'build']);
+});
+
+gulp.task('clean:dist', function () {
+    return del(['build/*', 'build', 'dist/*', 'dist']);
 });
 
 gulp.task('compile:test', ['clean:test'], function () {
@@ -18,12 +24,34 @@ gulp.task('compile:test', ['clean:test'], function () {
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('copy:test', ['clean:test'], function () {
-    return gulp.src(['app/views/*.html', 'app/components/**/*.html'], {base: '.'})
+gulp.task('compile:dist', ['clean:dist'], function () {
+    var options = {
+        target: 'es5',
+        module: 'commonjs'
+    };
+
+    return gulp.src(['app/**/*.ts'], {base: 'app'})
+        .pipe(typescript(options))
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('copy:test', ['clean:test'], function () {
+    return gulp.src(['app/views/*.html', 'app/components/**/*.html'], {base: ''})
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy:dist', ['clean:dist'], function () {
+    var templates = gulp.src(['app/views/*.html', 'app/components/**/*.html'], {base: 'app'})
+        .pipe(gulp.dest('build'));
+    var fonts = gulp.src(['bower_components/bootstrap/fonts/*.*', 'bower_components/fontawesome/fonts/*.*'], {base: '.'})
+        .pipe(flatten())
+        .pipe(gulp.dest('build/fonts'));
+
+    return merge(templates, fonts);
+});
+
 gulp.task('build:test', ['compile:test', 'copy:test']);
+gulp.task('build:dist', ['compile:dist', 'copy:dist']);
 
 gulp.task('test', ['build:test'], function (done) {
     new karmaServer({
