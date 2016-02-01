82704d326298484ac63e4ae32d5b9562377e3c72
Jakub Turek <ja@kubaturek.pl>
2016-02-01 21:23:15 +0100

    Process vendors as a separate script

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,6 @@ var merge = require('merge-stream');
 var flatten = require('gulp-flatten');
 var gutil = require('gulp-util');
 var debug = require('gulp-debug');
-var order = require('gulp-order');
-var gulpIf = require('gulp-if');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
@@ -97,9 +95,55 @@ gulp.task('assets', ['clean:dist', 'ngTemplates'], function () {
         sortOutput: true
     };
 
-    var scripts = gulp.src(['build/modules/*.js', 'app/**/*.ts'])
+    var vendorScripts = gulp.src([
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/angular/angular.min.js',
+            'bower_components/socket.io-client/socket.io.js',
+            'bower_components/angular-local-storage/dist/angular-local-storage.min.js',
+            'bower_components/angular-bootstrap/ui-bootstrap.js',
+            'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/lodash/lodash.min.js',
+            'bower_components/angular-emoji-filter/dist/emoji.min.js',
+            'bower_components/angular-ui-router/release/angular-ui-router.js',
+            'bower_components/showdown/src/showdown.js',
+            'bower_components/angular-markdown-directive/markdown.js',
+            'bower_components/angular-animate/angular-animate.js',
+            'bower_components/SoundJS/lib/soundjs-0.6.1.min.js',
+            'bower_components/angular-socket-io/socket.min.js',
+            'bower_components/angular-growl-v2/build/angular-growl.js'
+        ])
         .pipe(sourcemaps.init())
-        .pipe(gulpIf(/\.ts$/, typescript(tsOptions)))
+        .pipe(concat('vendor_scripts.js'))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/assets'));
+
+    var vendorStyles = gulp.src([
+            'bower_components/bootstrap/dist/css/bootstrap.css',
+            'bower_components/angular-growl-v2/build/angular-growl.css',
+            'bower_components/fontawesome/css/font-awesome.css',
+            'bower_components/angular-emoji-filter/dist/emoji.min.css'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('vendor_styles.css'))
+        .pipe(minifyCss())
+        .pipe(rev())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/assets'));
+
+    var templates = gulp.src(['build/modules/*.js'], {base: 'build'})
+        .pipe(sourcemaps.init())
+        .pipe(concat('templates.js'))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/assets'));
+
+    var scripts = gulp.src(['app/**/*.ts', 'app/modules/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsOptions))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rev())
@@ -115,7 +159,7 @@ gulp.task('assets', ['clean:dist', 'ngTemplates'], function () {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/assets'));
 
-    return merge(scripts, styles)
+    return merge(vendorScripts, vendorStyles, templates, scripts, styles)
         .pipe(rev.manifest())
         .pipe(gulp.dest('build'));
 });
