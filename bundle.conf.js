fc43b293a4c61ab4cb54ac27d4ba593298f7959c
Jakub Turek <ja@kubaturek.pl>
2016-02-27 12:37:36 +0100

    Create bundle-assets configuration file

---
+++ bundle.conf.js
@@ -0,0 +1,178 @@
+var typescript = require('gulp-typescript');
+var lazypipe = require('lazypipe');
+var less = require('gulp-less');
+var tsOptions = {
+        target: 'es5',
+        module: 'commonjs',
+        typescript: require('typescript'),
+        sortOutput: true,
+        removeComments: true
+    };
+var gulpIf = require('gulp-if');
+var scriptsPipeLine = lazypipe()
+    .pipe(function () {
+        return gulpIf(/\.ts$/, typescript(tsOptions));
+    });
+var stylesPipeline = lazypipe()
+    .pipe(function () {
+        return gulpIf(/\.less$/, less());
+    });
+
+var bundleOptions = {
+    pluginOptions: {
+        'gulp-sourcemaps': {
+            destPath: ''
+        }
+    },
+    result: {
+        type: {
+            scripts: function (path) {
+                return path;
+            },
+            styles: function (path) {
+                return path;
+            }
+        }
+    },
+    transforms: {
+        scripts: scriptsPipeLine,
+        styles: stylesPipeline
+    }
+};
+
+module.exports = {
+    bundle: {
+        popup_vendor: {
+            styles: [
+                "bower_components/bootstrap/dist/css/bootstrap.css",
+                "bower_components/angular-growl-v2/build/angular-growl.css",
+                "bower_components/fontawesome/css/font-awesome.css",
+                "bower_components/angular-emoji-filter/dist/emoji.min.css"
+            ],
+            scripts: [
+                "bower_components/jquery/dist/jquery.js",
+                "bower_components/angular/angular.min.js",
+                "bower_components/angular-local-storage/dist/angular-local-storage.min.js",
+                "bower_components/angular-bootstrap/ui-bootstrap.js",
+                "bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
+                "bower_components/angular-sanitize/angular-sanitize.js",
+                "bower_components/lodash/lodash.min.js",
+                "bower_components/angular-emoji-filter/dist/emoji.min.js",
+                "bower_components/angular-ui-router/release/angular-ui-router.js",
+                "bower_components/showdown/src/showdown.js",
+                "bower_components/angular-markdown-directive/markdown.js",
+                "bower_components/angular-animate/angular-animate.js"
+            ],
+            options: bundleOptions
+        },
+        popup: {
+            styles: [
+                "assets/less/styles.less",
+                "app/components/pull_requests_list_component/pull_requests_list_component.less",
+                "app/components/pull_requests_header_component/pull_requests_header_component.less",
+                "app/components/pull_request_component/pull_request_component.less",
+                "app/components/user_vote_component/user_vote_component.less",
+                "app/components/section_title_component/section_title_component.less",
+                "app/components/pull_request_link_component/pull_request_link_component.less",
+                "app/components/navigation_bar_component/navigation_bar_component.less",
+                "app/components/reminder_component/reminder_component.less",
+                "app/components/approval_progress_component/approval_progress_component.less",
+                "app/components/pull_request_preview_component/pull_request_preview_component.less",
+                "app/components/reviewer_component/reviewer_component.less"
+            ],
+            scripts: [
+                "app/components/pull_requests_list_component/pull_requests_list_component.ts",
+                "app/components/user_vote_component/user_vote_component.ts",
+                "app/components/pull_request_component/pull_request_component.ts",
+                "app/components/pull_requests_header_component/pull_requests_header_component.ts",
+                "app/components/approval_progress_component/approval_progress_component.ts",
+                "app/components/section_title_component/section_title_component.ts",
+                "app/components/pull_request_link_component/pull_request_link_component.ts",
+                "app/components/navigation_bar_component/navigation_bar_component.ts",
+                "app/components/navigation_brand_component/navigation_brand_component.ts",
+                "app/components/reminder_component/reminder_component.ts",
+                "app/components/pull_request_preview_component/pull_request_preview_component.ts",
+                "app/components/reviewer_component/reviewer_component.ts",
+                "app/services/models.ts",
+                "app/services/pull_request_repository.ts",
+                "app/services/config.ts",
+                "app/services/sound_repository.ts",
+                "app/services/factories.ts",
+                "app/filters/assigned_filter.ts",
+                "app/filters/authored_filter.ts",
+                "app/filters/unapproved_first_filter.ts",
+                "app/config/routing.ts",
+                "app/modules/bitbucket_notifier.ts",
+                "app/modules/templates.js"
+            ],
+            options: bundleOptions
+        },
+        background_vendor: {
+            scripts: [
+                "bower_components/socket.io-client/socket.io.js",
+                "bower_components/angular/angular.min.js",
+                "bower_components/angular-local-storage/dist/angular-local-storage.min.js",
+                "bower_components/angular-socket-io/socket.min.js",
+                "bower_components/lodash/lodash.min.js",
+                "bower_components/SoundJS/lib/soundjs-0.6.1.min.js"
+            ],
+            options: bundleOptions
+        },
+        background: {
+            scripts: [
+                "app/components/background_component/background_component.ts",
+                "app/services/socket_manager.ts",
+                "app/services/socket_handler.ts",
+                "app/services/notifier.ts",
+                "app/services/models.ts",
+                "app/services/pull_request_repository.ts",
+                "app/services/notification_repository.ts",
+                "app/services/config.ts",
+                "app/services/indicator.ts",
+                "app/services/factories.ts",
+                "app/services/sound_manager.ts",
+                "app/services/sound_repository.ts",
+                "app/modules/bitbucket_notifier_background.ts"
+            ],
+            options: bundleOptions
+        },
+        options_vendor: {
+            styles: [
+                "bower_components/bootstrap/dist/css/bootstrap.css",
+                "bower_components/angular-growl-v2/build/angular-growl.css",
+                "bower_components/fontawesome/css/font-awesome.css",
+                "bower_components/angular-emoji-filter/dist/emoji.min.css"
+            ],
+            scripts: [
+                "bower_components/jquery/dist/jquery.js",
+                "bower_components/angular/angular.min.js",
+                "bower_components/angular-local-storage/dist/angular-local-storage.min.js",
+                "bower_components/angular-growl-v2/build/angular-growl.js",
+                "bower_components/lodash/lodash.min.js",
+                "bower_components/SoundJS/lib/soundjs-0.6.1.min.js"
+            ],
+            options: bundleOptions
+        },
+        options: {
+            styles: [
+                "assets/less/styles.less",
+                "app/components/section_title_component/section_title_component.less",
+                "app/components/navigation_bar_component/navigation_bar_component.less",
+                "app/components/approval_progress_component/approval_progress_component.less"
+            ],
+            scripts: [
+                "app/components/options_component/options_component.ts",
+                "app/components/section_title_component/section_title_component.ts",
+                "app/components/navigation_bar_component/navigation_bar_component.ts",
+                "app/components/approval_progress_component/approval_progress_component.ts",
+                "app/services/config.ts",
+                "app/services/models.ts",
+                "app/services/sound_manager.ts",
+                "app/services/sound_repository.ts",
+                "app/modules/bitbucket_notifier_options.ts",
+                "app/modules/templates_options_module.js"
+            ],
+            options: bundleOptions
+        }
+    }
+};
