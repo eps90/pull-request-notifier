cc8a048a969cd5e596b6e6b0aefbf641f86620a2
Kuba Turek <ja@kubaturek.pl>
2017-07-30 21:47:24 +0200

    Unify webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,48 +1,73 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        popup: path.resolve(__dirname, '../app/modules/bitbucket_notifier.ts'),
-        background: path.resolve(__dirname, '../app/modules/bitbucket_notifier_background.ts'),
-        options: path.resolve(__dirname, '../app/modules/bitbucket_notifier_options.ts')
-    },
+module.exports = (function webpackConfig() {
+    const isProd = process.env.npm_lifecycle_event === 'build:prod';
+    const isTest = process.env.npm_lifecycle_event === 'test';
 
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: '[name].[hash].js',
-        publicPath: '/',
-        chunkFilename: '[name].[hash].js'
-    },
+    const config = {
+        entry: isTest ? void 0 : {
+            popup: path.resolve(__dirname, '../app/modules/bitbucket_notifier.ts'),
+            background: path.resolve(__dirname, '../app/modules/bitbucket_notifier_background.ts'),
+            options: path.resolve(__dirname, '../app/modules/bitbucket_notifier_options.ts')
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [
-                    path.resolve(__dirname, '../app')
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        exportAsEs6Default: true
+        output: isTest ? {} : {
+            path: path.resolve(__dirname, '../dist'),
+            filename: '[name].[hash].js',
+            publicPath: '/',
+            chunkFilename: '[name].[hash].js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader'
+                },
+                {
+                    test: /\.html$/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            exportAsEs6Default: true
+                        }
                     }
                 }
-            }
+            ]
+        },
+
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+
+        plugins: [
+            new webpack.ProvidePlugin({
+                'window.jQuery': 'jquery'
+            })
         ]
-    },
+    };
 
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
+    if (isTest) {
+        config.devtool = 'inline-source-map';
+    } else if (isProd) {
+        config.devtool = 'source-map';
+    } else {
+        config.devtool = 'eval-source-map';
+    }
 
-    plugins: [
-        new webpack.ProvidePlugin({
-            'window.jQuery': 'jquery'
-        })
-    ]
-};
+    if (isProd) {
+        config.plugins.push(
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common'
+            })
+        );
+        config.plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true
+            })
+        );
+    }
+
+    return config;
+})();
--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function(config) {
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
 
-    webpack: require('./config/webpack.test.config'),
+    webpack: require('./config/webpack.config'),
 
     client: {
       captureConsole: true
