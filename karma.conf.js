56663305e1a1d9b06cbb688742d0d927fab56691
Kuba Turek <ja@kubaturek.pl>
2017-07-30 11:50:49 +0200

    Adapt karma config to webpack

--- karma.conf.js
+++ karma.conf.js
@@ -20,27 +20,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/angular/angular.min.js',
-        'node_modules/angular-mocks/angular-mocks.js',
-        'node_modules/angular-local-storage/dist/angular-local-storage.min.js',
-        'node_modules/socket.io-client/dist/socket.io.js',
-        'node_modules/lodash/lodash.js',
-        'node_modules/angular-socket.io-mock/angular-socket.io-mock.js',
-        'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-        'node_modules/angular-growl-v2/build/angular-growl.js',
-        'bower_components/angular-emoji-filter/dist/emoji.min.js',
-        'node_modules/angular-sanitize/angular-sanitize.js',
-        'node_modules/createjs-soundjs/lib/soundjs-0.6.2.min.js',
-        'node_modules/angular-ui-router/release/angular-ui-router.js',
-        'node_modules/showdown/src/showdown.js',
-        'node_modules/angular-markdown-directive/markdown.js',
-        'node_modules/angular-animate/angular-animate.js',
-        'build/**/!(bitbucket_notifier*).js',
-        'build/app/modules/bitbucket_notifier.js',
-        'build/app/modules/bitbucket_notifier_background.js',
-        'build/app/modules/bitbucket_notifier_options.js',
-        'build/**/*.html'
+        'test/bootstrap.js'
     ],
 
 
@@ -52,15 +32,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "**/*.html": ['ng-html2js']
+        'test/bootstrap.js': ['webpack', 'sourcemap']
     },
-
-    ngHtml2JsPreprocessor: {
-        stripPrefix: "build/app/",
-        prependPrefix: "../",
-        moduleName: 'bitbucketNotifier.templates'
-    },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -92,6 +65,8 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+
+    webpack: require('./config/webpack.test.config'),
   })
 };
