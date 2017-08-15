0bd8eca714e1590622452125367741dc0c0dd0ce
Kuba Turek <ja@kubaturek.pl>
2017-08-15 21:58:59 +0200

    Add missing inject for NavigationBarController

--- app/components/navigation_bar_component/navigation_bar_controller.ts
+++ app/components/navigation_bar_component/navigation_bar_controller.ts
@@ -1,6 +1,8 @@
 export class NavigationBarController implements ng.IComponentController {
     public appVersion: string;
 
+    public static $inject: string[] = ['bitbucketUrl'];
+
     constructor(private bitbucketUrl: string) {}
 
     public $onInit = () => {
