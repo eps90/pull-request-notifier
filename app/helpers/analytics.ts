5039ad24727b66b8d6ca6908f5e50cce07b2f8c7
Kuba Turek <ja@kubaturek.pl>
2017-09-17 12:08:48 +0200

    [#3] Set up analytics config specific for extensions

--- app/helpers/analytics.ts
+++ app/helpers/analytics.ts
@@ -1,12 +1,24 @@
 export function setUpAnalytics(application: ng.IModule): void {
-    const GOOGLE_ANALYTICS_KEY = process.env.hasOwnProperty('GOOGLE_ANALYTICS_KEY')
-        ? process.env.GOOGLE_ANALYTICS_KEY
-        : '';
+    const GOOGLE_ANALYTICS_KEY = process.env.GOOGLE_ANALYTICS_KEY || '';
     if (GOOGLE_ANALYTICS_KEY.length > 0) {
         application.config([
             'AnalyticsProvider',
-            (analyticsProvider: angular.google.analytics.AnalyticsProvider) => {
-                analyticsProvider.setAccount(GOOGLE_ANALYTICS_KEY);
+            (analyticsProvider) => {
+                analyticsProvider
+                    .setAccount({
+                        tracker: GOOGLE_ANALYTICS_KEY,
+                        fields: {
+                            cookieDomain: 'none'
+                        },
+                        set: {
+                            checkProtocolTask: () => {}
+                        }
+                    })
+                    .enterDebugMode(DEV);
+
+                if (TEST) {
+                    analyticsProvider.enterTestMode();
+                }
             }
         ]);
     }
