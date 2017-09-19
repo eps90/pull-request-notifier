bab5491e1705411ad60b42f02515dff8f8106760
Kuba Turek <ja@kubaturek.pl>
2017-09-19 20:17:40 +0200

    [#3] Log tab switch events to ANALYTICS

--- app/components/pull_requests_list_component/pull_requests_list_controller.ts
+++ app/components/pull_requests_list_component/pull_requests_list_controller.ts
@@ -1,12 +1,19 @@
 import {PullRequestRepository} from '../../services/pull_request_repository';
 import {PullRequest} from '../../models/pull_request';
+import {TabSwitchEvent} from '../../models/analytics_event/tab_switch_event';
+import {AnalyticsEventDispatcher} from '../../services/analytics_event_dispatcher';
 
 export class PullRequestsListController implements ng.IComponentController {
     public pullRequests: PullRequest[];
 
-    public static $inject: string[] = ['PullRequestRepository'];
+    public static $inject: string[] = ['PullRequestRepository', 'AnalyticsEventDispatcher'];
 
-    constructor(private pullRequestRepository: PullRequestRepository) {}
+    private currentlyChosenTab: string;
+
+    constructor(
+        private pullRequestRepository: PullRequestRepository,
+        private analyticsEventDispatcher: AnalyticsEventDispatcher
+    ) {}
 
     public $onInit = () => {
         this.pullRequests = this.pullRequestRepository.pullRequests;
@@ -22,5 +29,29 @@ export class PullRequestsListController implements ng.IComponentController {
         //     },
         //     true
         // );
+    }
+
+    public onSelect(tabName: string): void {
+        if (this.currentlyChosenTab === undefined) {
+            // Initial tab select
+            this.currentlyChosenTab = tabName;
+            return;
+        } else if (tabName === this.currentlyChosenTab) {
+            return;
+        }
+
+        this.currentlyChosenTab = tabName;
+        let analyticsEvent: TabSwitchEvent;
+        switch (tabName) {
+            case 'AUTHORED':
+                analyticsEvent = TabSwitchEvent.authoredPullRequestsTab();
+                break;
+            case 'ASSIGNED':
+                analyticsEvent = TabSwitchEvent.assignedPullRequestsTab();
+                break;
+            default:
+                break;
+        }
+        this.analyticsEventDispatcher.dispatch(analyticsEvent);
     }
 }
