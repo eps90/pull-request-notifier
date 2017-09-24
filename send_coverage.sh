c29f9e91a2746490100ba58cb905a99d2e938dcd
Kuba Turek <ja@kubaturek.pl>
2017-09-24 18:07:24 +0200

    Exit from script with last status exit code

--- send_coverage.sh
+++ send_coverage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 node_modules/.bin/remap-istanbul -i build/coverage/json/coverage-final.json -o build/coverage/lcov.info -t lcovonly
-cat build/coverage/lcov.info | node_modules/.bin/codacy-coverage
+node_modules/.bin/codacy-coverage < build/coverage/lcov.info
+echo $?
