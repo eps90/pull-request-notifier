ca103ab4d907ebfac6928c45a6830cbcb42ddd36
Kuba Turek <ja@kubaturek.pl>
2017-11-10 18:48:49 +0100

    [#28] Track DND mode operations

--- app/services/do_not_disturb_service.ts
+++ app/services/do_not_disturb_service.ts
@@ -1,11 +1,14 @@
 import {Config} from './config';
 import {Duration} from './dnd/duration';
 import * as moment from 'moment';
+import {AnalyticsEventDispatcher} from './analytics_event_dispatcher';
+import {DndTurnedOnEvent} from '../models/analytics_event/dnd_turned_on_event';
+import {DndTurnedOffEvent} from '../models/analytics_event/dnd_turned_off_event';
 
 export class DoNotDisturbService {
-    public static $inject: string[] = ['Config'];
+    public static $inject: string[] = ['Config', 'AnalyticsEventDispatcher'];
 
-    constructor(private config: Config) {}
+    constructor(private config: Config, private analyticsEventDispatcher: AnalyticsEventDispatcher) {}
 
     public turnOnDnd(duration: Duration): void {
         const currentDate = moment();
@@ -15,10 +18,12 @@ export class DoNotDisturbService {
         );
 
         this.config.setDndToTime(dndEndTime.valueOf());
+        this.analyticsEventDispatcher.dispatch(DndTurnedOnEvent.turnedOnFor(duration));
     }
 
     public turnOffDnd(): void {
         this.config.clearDndToTime();
+        this.analyticsEventDispatcher.dispatch(new DndTurnedOffEvent());
     }
 
     public isDndOn(): boolean {
