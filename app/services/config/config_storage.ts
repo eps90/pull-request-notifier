e50bfe9f55fc22ffdc25166624c683782d4a41c0
Kuba Turek <ja@kubaturek.pl>
2017-11-17 21:35:22 +0100

    Create config storage interface

---
+++ app/services/config/config_storage.ts
@@ -0,0 +1,7 @@
+export interface ConfigStorageInterface {
+    getItem(itemKey: string): any;
+    hasItem(itemKey: string): boolean;
+    removeItem(itemKey: string): void;
+    allItems(): Map<string, any>;
+    save(config: Map<string, any>): void;
+}
