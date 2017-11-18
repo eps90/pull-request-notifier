7d836cf86f8f07c90a9764d9ae0dcedda6c5724a
Kuba Turek <ja@kubaturek.pl>
2017-11-18 18:30:54 +0100

    Add missing service definitions

--- app/modules/eps_config.ts
+++ app/modules/eps_config.ts
@@ -1,6 +1,9 @@
 import * as angular from 'angular';
 import {AngularLocalStorageStorage} from '../services/config/angular_localstorage_storage';
+import {ConfigProvider} from '../services/config/config_provider';
 
 const application = angular.module('eps.config', ['LocalStorageModule']);
 
 application.service('config.storage.ngLocalStorage', AngularLocalStorageStorage);
+application.service('config.storage', AngularLocalStorageStorage);
+application.provider('config', ConfigProvider);
