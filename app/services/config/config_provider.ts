ad4c94056bf9f18f35fb5e43e022c0a852944422
Kuba Turek <ja@kubaturek.pl>
2017-11-19 11:33:55 +0100

    Allow to set custom storage in ConfigProvider

--- app/services/config/config_provider.ts
+++ app/services/config/config_provider.ts
@@ -3,12 +3,19 @@ import {Config} from './config';
 
 export class ConfigProvider implements ng.IServiceProvider {
     public $get = ['config.storage', (storage: ConfigStorageInterface) => {
-        return new Config(storage, this.serviceDefaults || new Map());
+        const configStorage = this.customStorage || storage;
+        const defaults = this.serviceDefaults || new Map();
+        return new Config(configStorage, defaults);
     }];
 
     private serviceDefaults: Map<string, any>;
+    private customStorage: ConfigStorageInterface;
 
     public setDefaults(defaults: Map<string, any>): void {
         this.serviceDefaults = defaults;
+    }
+
+    public useCustomStorage(storage: ConfigStorageInterface) {
+        this.customStorage = storage;
     }
 }
